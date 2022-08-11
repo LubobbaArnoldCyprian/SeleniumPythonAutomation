5c4d49a1f076e78645f3168054694e3c9fd232e2
Lubobba arnold Cyprian <lubobba@outlook.com>
2022-08-11 14:52:43 +0300

    Added utilities and configurations folders

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture()
 def setup():
-    driver = webdriver.Chrome('/Users/carl/Desktop/Q.A/chromedriver')
+    # driver = webdriver.Chrome('/Users/carl/Desktop/Q.A/chromedriver')
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     return driver
--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,24 +1,29 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
-
+from testCases.conftest import setup
 from pageObjects.LoginPage import LoginPage
+from utilities.readProperties import ReadConfig
 
 
 class Test_001_Login:
-    baseURL = "https://admin-demo.nopcommerce.com"
-    username = "admin@yourstore.com"
-    password = "admin"
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
 
     def test_homePageTitle(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
-        self.driver.close()
-        if act_title == "Your store. Login":
-            assert True
-        else:
+        print(act_title)
+        if act_title != "Your store. Login1":
+            self.driver.save_screenshot(".//Screenshots/"+"self.test_homePageTitle.png")
+            self.driver.close()
             assert False
+        else:
+            assert True
+        self.driver.close()
+
 
     def test_login(self, setup):
         self.driver = setup
